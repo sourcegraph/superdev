65d0e6d5f93952773a00049117b91cf1226527b3
Lewis Bonathan Metcalf <lewis.metcalf@sourcegraph.com>
2025-04-17 12:42:45 +0200

    wip

--- ui/src/components/ThreadList.js
+++ ui/src/components/ThreadList.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import ThreadCard from './ThreadCard';
+import ThreadListItem from './ThreadListItem';
 
-function ThreadList({ threads, outputRefs }) {
+function ThreadList({ threads, onThreadSelect }) {
   return (
-    <div className="threads-grid">
-      {threads.map((thread) => (
-        <ThreadCard key={thread.thread_id} thread={thread} outputRefs={outputRefs} />
-      ))}
-
-      {threads.length === 0 && (
-        <div>No threads found. Start a new thread to see output.</div>
+    <div className="threads-list">
+      {threads.length > 0 ? threads.map((thread) => (
+        <ThreadListItem 
+          key={thread.thread_id} 
+          thread={thread} 
+          onClick={() => onThreadSelect(thread)} 
+        />
+      )) : (
+        <div className="no-threads-message">
+          No threads found. Start a new thread to see output.
+        </div>
       )}
     </div>
   );
